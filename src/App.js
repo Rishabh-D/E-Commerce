dc606c92101cd4e22eec6cbb33eec3c11035ebb6
Rishabh-D <dubeyrishabh.rd@gmail.com>
2023-09-13 16:51:56 +0530

    login-signup pages added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,37 @@ import "./App.css";
 import ProductList from "./features/product/ProductList";
 import Navbar from "./features/navbar/Navbar";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+
+import { createRoot } from "react-dom/client";
+import SignupPage from "./pages/SignupPage";
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/signup",
+        element: <SignupPage />,
+    },
+]);
 
 function App() {
     return (
         <div className="App">
-            <Home></Home>
+            <RouterProvider router={router} />
         </div>
     );
 }
