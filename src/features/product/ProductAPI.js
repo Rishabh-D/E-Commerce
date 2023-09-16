49853782401d3c419ddc92b9abbeaaf854a4a9d6
Rishabh-D <dubeyrishabh.rd@gmail.com>
2023-09-17 00:08:27 +0530

    brands and catergory api added. Fixed next, previous  buttons

--- src/features/product/ProductAPI.js
+++ src/features/product/ProductAPI.js
@@ -1,55 +1,63 @@
 // A mock function to mimic making an async request for data
 export function fetchAllProducts() {
-    return new Promise(async (resolve) => {
-        console.log("inside a call");
-        const response = await fetch("http://localhost:8080/products");
-        const data = await response.json();
-        console.log(data);
-        resolve({ data });
-    });
+	console.log("this will never be called");
+	return new Promise(async (resolve) => {
+		console.log("inside a call");
+		const response = await fetch("http://localhost:8080/products");
+		const data = await response.json();
+		console.log(data);
+		resolve({ data });
+	});
 }
 
 export function fetchProductsByFilters({ filter, sort, pagination }) {
-    // filter ={"brand":["apple", "samsung"], "key2":[val1]}
-    let queryString = "";
-    console.log("sort inside fetch:pagination", pagination);
-    // console.log(filter, "lol", Object.keys(filter));
-    for (let key in filter) {
-        if (key === "_sort" || key === "_order") continue;
-        // console.log("here", key);
-        let query = `${key}=`;
-        for (let val of filter[key]) {
-            query += `${val},`;
-        }
-        queryString += query.slice(0, -1) + "&";
-    }
-    // queryString = queryString.slice(0, -1);
+	// filter ={"brand":["apple", "samsung"], "key2":[val1]}
+	let queryString = "";
+	console.log("sort inside fetch:pagination", pagination);
+	// console.log(filter, "lol", Object.keys(filter));
+	for (let key in filter) {
+		if (key === "_sort" || key === "_order") continue;
+		// console.log("here", key);
+		let query = `${key}=`;
+		for (let val of filter[key]) {
+			query += `${val},`;
+		}
+		queryString += query.slice(0, -1) + "&";
+	}
 
-    // if (filter._sort) {
-    // 	if (filter._order) {
-    // 		queryString += `&_sort=${filter._sort}&_order=${filter._order}`;
-    // 	} else {
-    // 		queryString += `&_sort=${filter._sort}`;
-    // 	}
-    // } else {
-    // 	if (filter._order) {
-    // 		queryString += `&_order${filter._order}`;
-    // 	}
-    // }
+	queryString += `_sort=${sort._sort}&_order=${sort._order}&`;
+	queryString += `_page=${pagination._page}&_limit=${pagination._limits}`;
 
-    queryString += `_sort=${sort._sort}&_order=${sort._order}&`;
-    queryString += `_page=${pagination._page}&_limit=${pagination._limits}`;
+	console.log("queryString:", queryString);
 
-    console.log("queryString:", queryString);
+	return new Promise(async (resolve) => {
+		// console.log("inside a call");
+		const response = await fetch(
+			`http://localhost:8080/products?` + queryString
+		);
+		const data = await response.json();
+		const totalItems = await response.headers.get("X-Total-Count");
+		// console.log(data);
+		resolve({ data: { products: data, totalItems } });
+	});
+}
 
-    return new Promise(async (resolve) => {
-        // console.log("inside a call");
-        const response = await fetch(
-            `http://localhost:8080/products?` + queryString
-        );
-        const data = await response.json();
-        const totalItems = await response.headers.get("X-Total-Count");
-        // console.log(data);
-        resolve({ data: { products: data, totalItems } });
-    });
+export function fetchCategories() {
+	return new Promise(async (resolve) => {
+		console.log("inside fetchCategories call");
+		const response = await fetch("http://localhost:8080/categories");
+		const data = await response.json();
+		console.log(data);
+		resolve({ data });
+	});
+}
+
+export function fetchBrands() {
+	return new Promise(async (resolve) => {
+		console.log("inside fetchBrands call");
+		const response = await fetch("http://localhost:8080/brands");
+		const data = await response.json();
+		console.log(data);
+		resolve({ data });
+	});
 }
